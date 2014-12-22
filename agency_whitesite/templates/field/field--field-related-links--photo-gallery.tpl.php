351d9943b51c42eec5ca98c77781f32d604fc7ad
Paul <pkil@2327222.no-reply.drupal.org>
2014-12-23 09:57:08 +1100

    new template for related urls

---
+++ agency_whitesite/templates/field/field--field-related-links--photo-gallery.tpl.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @file
+ * Overridden template implementation to display the value of a field.
+ */
+?>
+<div class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <?php if (!$label_hidden): ?>
+    <div class="field-label" <?php print $title_attributes; ?>><?php print $label ?></div>
+  <?php endif; ?>
+  <ul class="field-items">
+    <?php foreach ($items as $delta => $item): ?>
+	  <li class="field-item"><?php print $item_attributes[$delta]; ?><?php print render($item); ?></li>
+    <?php endforeach; ?>
+    </ul>
+</div>
