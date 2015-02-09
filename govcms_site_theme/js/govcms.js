8e4e07d6e64a763620c929c373527e969c73e93d
Paul <pkil@2327222.no-reply.drupal.org>
2015-02-09 12:40:58 +1100

    add footer stuff

--- govcms_site_theme/js/govcms.js
+++ govcms_site_theme/js/govcms.js
@@ -1,7 +1,7 @@
 (function ($) {
     $(document).ready(function () {
         console.log("Here");
-        $('body').append("<div id='footer-fixed' class='read-more'><a href='/plans/signup'>Sign up now</a></div>");
+        $('body').append("<div id='fixed-footer' class='read-more'><a href='/plans/signup'>Sign up now</a></div>");
         console.log("Again");
     });
 })(jQuery);
