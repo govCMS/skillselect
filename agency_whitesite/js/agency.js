773c33a9179508a65f2c97f360d270f344e07d42
Paul <pkil@2327222.no-reply.drupal.org>
2015-02-18 15:01:36 +1100

    working on the tag font size

--- agency_whitesite/js/agency.js
+++ agency_whitesite/js/agency.js
@@ -6,6 +6,32 @@
  * the README.txt next to this file.
  */
 
+function CalculateWeights(arrValues, weightsCount) {
+    var arrWeights = [];
+    var minValue = 999999;
+    var maxValue = -1;
+    for (var i = 0; i < arrValues.length; i++) {
+        var curValue = arrValues[i];
+        if (curValue < minValue)
+            minValue = curValue;
+        if (curValue > maxValue)
+            maxValue = curValue;
+    }
+
+    var diff = weightsCount / (maxValue - minValue);
+    for (var i = 0; i < arrValues.length; i++) {
+        var curValue = arrValues[i];
+        if (curValue == minValue)
+            arrWeights.push(1);
+        else if (curValue == maxValue)
+            arrWeights.push(weightsCount);
+        else
+            arrWeights.push(parseInt(curValue * diff, 10) + 1);
+    }
+
+    return arrWeights;
+}
+
 // JavaScript should be made compatible with libraries other than jQuery by
 // wrapping it with an "anonymous closure". See:
 // - https://drupal.org/node/1446420
@@ -30,33 +56,18 @@ Drupal.behaviors.my_custom_behavior = {
     $(document).ready(function () {
         var numberArray = [];
         var exists = false;
+        var count = 0;
         $('div.row-of-tags').each(function () {
             var occurance = $('span', this).attr('class');
             var number = occurance.replace('count-', '');
             numberArray.push(number);
-            exists = true;
+            count++;
         });
-        if(exists) {
-            var counts = {};
-            for (var i = 0; i < numberArray.length; i++) {
-                var num = numberArray[i];
-                counts[num] = counts[num] ? counts[num] + 1 : 1;
-            }
-            var highest = 0;
-            var lowest = 1000;
-            var count = 0;
-            for (var key in counts) {
-                count++;
-                console.log("Value: " + key + ":" + counts[key]);
-                if (counts[key] > highest) {
-                    highest = key;
-                }
-                if (counts[key] < lowest) {
-                    lowest = key;
-                }
-            }
-            console.log("Highest: " + highest);
-            console.log("Lowest: " + lowest);
+        if(count > 0) {
+            console.log(numberArray);
+            var tags = CalculateWeights(numberArray, count);
+            console.log(tags);
+/*
             $('div.row-of-tags').each(function () {
                 var number = $('span', this).attr('class').replace('count-', '');
                 var occurances = counts[number];
@@ -79,7 +90,7 @@ Drupal.behaviors.my_custom_behavior = {
                 }
 
                 $('span', this).attr('style', 'font-size:' + (style+70) + '%;');
-            });
+            });*/
         }
     });
 })(jQuery);
