30027a360590bf8ed1584c25d2eb229a043b5aaf
Paul <pkil@2327222.no-reply.drupal.org>
2015-02-18 14:49:46 +1100

    working on the tag font size

--- agency_whitesite/js/agency.js
+++ agency_whitesite/js/agency.js
@@ -59,25 +59,24 @@ Drupal.behaviors.my_custom_behavior = {
             console.log("Highest: " + highest);
             console.log("Lowest: " + lowest);
             $('div.row-of-tags').each(function () {
-                var occurance = $('span', this).attr('class');
-                var number = occurance.replace('count-', '');
-                counts[number];
+                var number = $('span', this).attr('class').replace('count-', '');
+                var occurances = counts[number];
                 var style = 30;
                 if (number == highest) {
                     style = 130;
                 } else if (number == lowest) {
                     style = 0;
                 } else {
-                    var diff = count / (highest - lowest);
+                    var diff = occurances / (highest - lowest);
                     console.log("Difference: " + diff);
                     console.log("Number: " + number);
 
 
 
-                    var fontSize = parseInt(number * diff, 100) + 1;
+                    var fontSize = parseInt(number * diff, 10) + 1;
                     console.log("Font Size: " + fontSize);
 
-                    style = 0;
+                    style = fontSize;
                 }
 
                 $('span', this).attr('style', 'font-size:' + (style+70) + '%;');
