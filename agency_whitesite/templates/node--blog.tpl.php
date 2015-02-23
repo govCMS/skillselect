333891f6a7004010305c7e39035860e5afdce0b7
Tim Hurley <tim.hurley@finance.gov.au>
2015-02-24 09:24:01 +1100

    blog datestamp

--- agency_whitesite/templates/node--blog.tpl.php
+++ agency_whitesite/templates/node--blog.tpl.php
@@ -2,10 +2,10 @@
   <p class="submitted">
     Posted on
     <time pubdate datetime="<?php print $submitted_pubdate; ?>">
-    <?php $date = date("d.n.Y", $node->created); ?>
-    <?php print "<br>".$date; ?>
+      <?php $date = date("d.n.Y", $node->created); ?>
+      <?php print "<br>".$date; ?>
     </time>
     by
-    <?php print theme('username', $node); ?>
+    <?php //print theme('username', $node); ?>
   </p>
 <?php endif; ?>
