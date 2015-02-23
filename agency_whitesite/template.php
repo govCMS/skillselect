8152cb2ad3b41aa1e0cb2c023c2344849cbc53fb
Tim Hurley <tim.hurley@finance.gov.au>
2015-02-24 09:14:49 +1100

    function for datestamp

--- agency_whitesite/template.php
+++ agency_whitesite/template.php
@@ -554,3 +554,9 @@ function agency_whitesite_preprocess_menu_link(&$variables, $hook) {
     $variables['element']['#localized_options']['html'] = TRUE;
   }
 }
+
+
+
+function agency_whitesite_preprocess_node(&$vars, $hook) {
+  $vars['submitted'] = date("l, M jS, Y", $vars['created']);
+}
