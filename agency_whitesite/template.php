7a3a73c2290f30c2783d89f3c146397271fa72da
Tim Hurley <tim.hurley@finance.gov.au>
2015-02-24 10:59:14 +1100

    blog datestamp

--- agency_whitesite/template.php
+++ agency_whitesite/template.php
@@ -558,5 +558,5 @@ function agency_whitesite_preprocess_menu_link(&$variables, $hook) {
 
 /// make the timestamp pretty
 function agency_whitesite_preprocess_node(&$vars, $hook) {
-  $vars['submitted'] = "Posted on " . date("l, M jS, Y", $vars['created']) . " at " . date("h:i A", $vars['created']) . " by " . $vars['name'];
+  $vars['submitted'] = "Posted on " . date("l, M jS, Y", $vars['created']) . " at " . date("h:i a", $vars['created']) . " by " . $vars['name'];
 }
