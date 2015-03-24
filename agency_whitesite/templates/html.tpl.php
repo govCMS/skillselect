2d84e37a74abd94f608e9509a31e7f74736e6ad2
paulk <paul.kilpatrick@finance.gov.au>
2015-03-24 17:17:43 +1100

    fix skip to menu link

--- agency_whitesite/templates/html.tpl.php
+++ agency_whitesite/templates/html.tpl.php
@@ -42,6 +42,7 @@
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
 <div id="skip-link">
+    <a href="#block-superfish-1" class="element-invisible element-focusable"><?php print t('Skip to menu'); ?></a>
     <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
 </div>
 <?php print $page_top; ?>
