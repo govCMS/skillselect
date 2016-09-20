8453f0044ac032919470f3761e8a36fc0c04e915
Paul Kilpatrick <paulkilpatrick@MacBookPRO.gateway>
2016-09-20 13:25:55 +1000

    Initial commit

--- govcms_site_theme/js/govcms.js
+++
@@ -1,5 +0,0 @@
-(function ($) {
-    $(document).ready(function () {
-        $('body').append("<div id='fixed-footer' class='read-more'><div><a href='/plans/signup'>Sign up now</a></div></div>");
-    });
-})(jQuery);
