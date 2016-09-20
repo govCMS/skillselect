aed346b3aa3dc83ad178ced94babefab190d2983
Paul Kilpatrick <paulkilpatrick@MacBookPRO.gateway>
2016-09-20 14:09:20 +1000

    sidebar_right

---
+++ agency_site/theme-settings.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @file
+ * Contains theme-settings form.
+ */
+
+/**
+ * Implements hook_form_system_theme_settings_alter().
+ */
+function agency_site_form_system_theme_settings_alter(&$form, &$form_state, $form_id = NULL) {
+}
