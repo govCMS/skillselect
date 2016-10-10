b858bd232a51e4deebb6936f9e36942bd57d6af5
Tim <tim@timhurley.net>
2016-10-11 10:45:19 +1100

    updated header img markup
    requires gulp to generate images from src still though

--- govstrap/templates/page.tpl.php
+++ govstrap/templates/page.tpl.php
@@ -12,7 +12,7 @@
         <a href="/"><img src="<?php print '/'.drupal_get_path('theme', 'govstrap').'/img/coas.svg';?>" height="64" alt="Skillselect Home" /></a>
       </div>
       <div class="col-md-6 text-right" id="logo">
-        <a href="/"><img src="<?php print '/'.drupal_get_path('theme', 'govstrap').'/img/skillselect@2x.png';?>" height="85" alt="Skillselect Home" /></a>
+        <a href="/"><img src="<?php print '/'.drupal_get_path('theme', 'govstrap').'/img/skillselect.png';?>" srcset="<?php print '/'.drupal_get_path('theme', 'govstrap').'/img/skillselect-hidpi.png';?> 2x" height="85" alt="Skillselect Home" /></a>
       </div>
     </div>
   </div>
